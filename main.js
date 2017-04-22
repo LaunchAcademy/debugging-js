bc31e360fc01d2436f9b9adc97019e4ee52aeaab
Dan Pickett <dan.pickett@launchacademy.com>
2017-04-22 15:12:46 -0400

    start of video

---
+++ main.js
@@ -0,0 +1,16 @@
+/*
+# Learning Goals
+- Debug JavaScript programs in the browser using Chrome Developer Tools
+- Debug JavaScript programs at the command line using node debug
+- Debug JavaScript programs at the command line using node --inspect --debug-brk
+*/
+
+let names = ['Sally', 'Jesse', 'John']
+
+for(let i = 0; i < names.length; i++) {
+  console.log(names[i])
+}
+
+// if(names === 'Jesse') {
+//
+// }
